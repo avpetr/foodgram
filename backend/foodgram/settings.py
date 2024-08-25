01b674cf9ad0b13ef8af9b2eb059e10b37a46814
Alex <avpetr@yandex.ru>
2024-08-26 02:31:41 +0300

    help 10

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -11,7 +11,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.getenv('SECRET_KEY', get_random_secret_key())
 
-DEBUG = os.getenv('DEBUG','').lower() == 'true'
+# DEBUG = os.getenv('DEBUG','').lower() == 'true'
+DEBUG = True
 BASE_URL = "http://127.0.0.1:8000/"
 ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS','localhost,127.0.0.1,158.160.83.152,avpetr.zapto.org;').split(',')
 
