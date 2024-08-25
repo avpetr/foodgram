c13c46c1e9fa3bc619d989c58f0ca12c2703b0a5
Alex <avpetr@yandex.ru>
2024-08-26 02:40:12 +0300

    help 10

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -10,7 +10,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 SECRET_KEY = os.getenv('SECRET_KEY', get_random_secret_key())
-
 # DEBUG = os.getenv('DEBUG','').lower() == 'true'
 DEBUG = True
 BASE_URL = "http://127.0.0.1:8000/"
