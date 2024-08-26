5454037061cff5cd034d6b01216a715dce9919b8
Alex <avpetr@yandex.ru>
2024-08-26 17:59:26 +0300

    csrf setting again...

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -14,7 +14,7 @@ SECRET_KEY = os.getenv('SECRET_KEY', get_random_secret_key())
 DEBUG = False
 BASE_URL = "http://127.0.0.1:8000/"
 ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS','localhost,127.0.0.1').split(',')
-CSRF_TRUSTED_ORIGINS = os.getenv('ALLOWED_HOSTS','localhost,127.0.0.1').split(',')
+CSRF_TRUSTED_ORIGINS = ['https://avpetr.zapto.org','https://*.127.0.0.1']
 
 
 INSTALLED_APPS = [
